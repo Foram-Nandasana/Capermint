8ea4d4b1bc7efbaaca1bb1811ef63fd93dbb62fe
Foram-Nandasana <d21it177@charusat.edu.in>
2024-01-31 00:10:08 +0530

    [WIP] Item

--- src/components/common/Item.js
+++ src/components/common/Item.js
@@ -3,35 +3,77 @@ import { useParams } from 'react-router-dom';
 import data from './Data.json';
 
 export const Item = ({  }) => {
-  const { id } = useParams();
-  const [matchingData, setMatchingData] = useState([]);
 
-  useEffect(() => {
+    const { id } = useParams();
     const ids = Number (id)
-    const filteredData = data.filter(item => item.id === ids);
-    setMatchingData(filteredData);
-    console.log(typeof id);
-    console.log(typeof ids);
-    console.log(filteredData, "filteredData");
-    console.log(matchingData, "matchingData");
+    const selectedItem = data.find(item => item.id === ids);
+  
+    return (
+      <div>
+        {selectedItem ? (
+          <div>
+            <h1>{selectedItem.id}</h1>
+            <h3>{selectedItem.title}</h3>
+            <p>{selectedItem.des}</p>
+            <h3>{selectedItem.price}</h3>
+          </div>
+        ) : (
+          <p>Data not found</p>
+        )}
+      </div>
+    );
 
-  }, [id, data]);
+        }
 
-  return (
-    <div>
-      {matchingData.length === 0 ? (
-        <h2>Data not found</h2>
-      ) : (
-        <>
-         <h2>{matchingData[0].id}</h2>
-          <h3>{matchingData[0].title}</h3>
-          <p>{matchingData[0].des}</p>
-          <h3>{matchingData[0].price}</h3>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   const { id } = useParams();
+//   const [matchingData, setMatchingData] = useState([]);
+
+//   useEffect(() => {
+//     const ids = Number (id)
+//     const filteredData = data.filter(item => item.id === ids);
+//     setMatchingData(filteredData);
+//     console.log(typeof id);
+//     console.log(typeof ids);
+//     console.log(filteredData, "filteredData");
+//     console.log(matchingData, "matchingData");
+
+//   }, [id, data]);
+
+//   return (
+//     <div>
+//       {matchingData.length === 0 ? (
+//         <h2>Data not found</h2>
+//       ) : (
+//         <>
+//          <h2>{matchingData[0].id}</h2>
+//           <h3>{matchingData[0].title}</h3>
+//           <p>{matchingData[0].des}</p>
+//           <h3>{matchingData[0].price}</h3>
           
-        </>
-      )}
-    </div>
+//         </>
+//       )}
+//     </div>
 
-  );
-}
+//   );
+
 
