cd9b697263898ae553419699c39a1cd1cbf41869
Foram-Nandasana <d21it177@charusat.edu.in>
2024-02-07 18:57:46 +0530

    [WIP] Add product dialog

--- src/components/common/Product.js
+++ src/components/common/Product.js
@@ -1,6 +1,6 @@
-import React from 'react'
+// import React from 'react'
 import Data from './Data.json'
-import { Button } from '@material-ui/core';
+// import { Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import './Font.css';
 import { useNavigate } from 'react-router-dom'
@@ -8,6 +8,15 @@ import Typo from './component/Typo';
 import Card from './component/Card';
 import { useDispatch } from 'react-redux';
 import { addToCard } from '../../store/slices/productSlice';
+
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
 
 const useStyles = makeStyles(() => ({
     mainContain: {
@@ -52,23 +61,49 @@ const useStyles = makeStyles(() => ({
         border: 'none',
         width: '100px',
         fontSize: 'clamp(8px, 1.2vw , 25px)',
+    },
+    container1: {
+        flexWrap: 'wrap',
+        padding: '16px',
+        backgroundColor: '#f1f1f1',
+        width: '50vw'
       },
 }));
 
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
 export const Product = () => {
+
+
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+
+
 
     const dispatch = useDispatch();
     const classes = useStyles();
     const navigate = useNavigate();
+
 
     const addCard = (value) => {
         console.log(value, "product");
         dispatch(addToCard(value));
     };
 
-const handleForm = () => {
-    navigate(`/ProductForm`)
-}
+    const handleForm = () => {
+        navigate(`/ProductForm`)
+    }
 
     const handleCard = (data) => {
         navigate(`/Product/${data}`);
@@ -78,7 +113,49 @@ const handleForm = () => {
         <div className={classes.mainContain}>
             <div className='ObjectSansRegular' >
                 <h2 className={classes.title} >Products</h2>
-                <button className={classes.button} onClick={() => handleForm()}>Add Product</button>
+                {/* <button className={classes.button} onClick={() => handleForm()}>Add Product</button> */}
+
+                <React.Fragment>
+                    <button className={classes.button} onClick={handleClickOpen}>Add Product</button>
+                    {/* <Button variant="outlined" onClick={handleClickOpen}>
+                        Slide in alert dialog
+                    </Button> */}
+                    <Dialog
+                        open={open}
+                        TransitionComponent={Transition}
+                        keepMounted
+                        onClose={handleClose}
+                        aria-describedby="alert-dialog-slide-description"
+                    >
+                        <form>
+                            <div className={classes.container1}>
+                                <label for="pname"><b>Product Name</b></label>
+                                <input type="text"
+                                placeholder="Enter Product Name"
+                                    name="pname"
+                                    required
+                                />
+                                <label for="description"><b>Product Description</b></label>
+                                <input type="text"
+                                placeholder="Enter Product Description"
+                                    name="description"
+                                    required
+                                />
+                                <label for="price"><b>Product Price</b></label>
+                                <input type="text"
+                                placeholder="Enter Product Price"
+                                    name="price"
+                                    required
+                                />
+                            </div>
+                        </form>
+                        <DialogActions>
+                            <Button onClick={handleClose}>Disagree</Button>
+                            <Button onClick={handleClose}>Agree</Button>
+                        </DialogActions>
+                    </Dialog>
+                </React.Fragment>
+
                 <div className={classes.Container}>
                     {Data.map((result, index) => (
                         <Card variant="card" className={classes.card} key={index} >
@@ -100,9 +177,11 @@ const handleForm = () => {
                         </Card>
                     ))}
                 </div>
-    
-               
+
+
             </div>
+
+
 
         </div>
     )
